b619ec1f66deaba91e3e1057cd761a977842ed3c
SamTanFL <samsite_tan@hotmail.com>
2020-02-27 16:34:13 +0800

    mvp done

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,10 +2,49 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+            toDo: [],
+            words: ""
+        }
+    }
+
+    changeHandler(event) {
+        this.setState({words:event.target.value})
+    }
+
+    clickHandler() {
+        this.state.toDo.push(this.state.words)
+        let list = this.state.toDo
+        this.setState(
+            {
+                toDo: list,
+                words:""
+            }
+        )
+    }
+
   render() {
+    let index = 0
+    const doElements = this.state.toDo.map(listItem => {
+        index++
+        return <li key={index - 1}>{listItem}</li>
+    })
+
     return (
       <div>
-        Welcome.
+        <div className="item">
+            <h1>{this.state.word}</h1>
+            <input onChange={(event)=>{this.changeHandler(event);}} value={this.state.words} />
+        </div>
+        <button onClick={()=>{ this.clickHandler() }}>Add to To-Do List</button>
+        <div className="todo">
+            <ol>
+                {doElements}
+            </ol>
+        </div>
       </div>
     );
   }
